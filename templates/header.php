8bd52742b7949c1589ff968a36303ba7a9ccae07
Emma Sneller <emma@Emmas-Air.home>
2022-04-24 17:56:05 +0100

    header changes

--- templates/header.php
+++ templates/header.php
@@ -2,12 +2,15 @@
 
 <html>
     <head>
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="<?php echo URL .'/assets/images/logoiconweb.png' ?> ">
         <!-- Styles -->
         <link rel="stylesheet" href="<?php echo URL .'/assets/styles/style.css'?>">
             <!-- Add the slick-theme.css if you want default styling -->
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
             <!-- Add the slick-theme.css if you want default styling -->
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+        <link rel="stylesheet" href="'https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"/>
         <!-- JS -->
         <script src="https://kit.fontawesome.com/c3010dc747.js" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -17,6 +20,6 @@
     <body>
         <!-- Header Container - Site Name/Logo and Menu -->
       <header id="main-header" class="container flexContainer">
-          <a href="#"><img class="logoImg" alt="Giph Logo" src="<?php echo URL .'/assets/images/giphyLogo.png'?>" /></a>
+          <img class="logoImg" alt="EMA Web Logo" src="<?php echo URL .'/assets/images/logoiconweb.png'?>" />
       </header>
    
